8bc237f7ffa3a71a5525b00144404a15f5369858
rochoa13 <ochoarafael.jr@gmail.com>
2023-02-02 18:38:38 +0000

    Complete instantiationExample

--- src/main/java/InstantiationExample.java
+++ src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+       Object reference = new Object();
+        return reference;
     }
 }
